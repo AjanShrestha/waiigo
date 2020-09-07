529cd3eb5105ab11d044dcae18f9a324757385e6
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-09-07 20:07:51 +1000

    feat: Let Statement and Identifier

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "monkey/token"
+
 // The base Node interface
 type Node interface {
 	TokenLiteral() string
@@ -28,3 +30,21 @@ func (p *Program) TokenLiteral() string {
 		return ""
 	}
 }
+
+// Statements
+type LetStatement struct {
+	Token token.Token // the token.Let Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
