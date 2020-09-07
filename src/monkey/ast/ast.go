106f2d8fcab243759d13569dca8bcd78f8f5d151
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-09-07 19:33:55 +1000

    feat: Basic building block of AST

---
+++ src/monkey/ast/ast.go
@@ -0,0 +1,30 @@
+package ast
+
+// The base Node interface
+type Node interface {
+	TokenLiteral() string
+}
+
+// All statement nodes implement this
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// All expression nodes implement this
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
